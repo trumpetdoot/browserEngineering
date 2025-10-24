12ff6da79036e361bb3fdabd69281d06505bc277
Andy Yin <andyyin06@gmail.com>
2025-10-23 21:00:15 -0400

    Added https and custom port support

--- telnet.py
+++ telnet.py
@@ -1,23 +1,39 @@
 import socket
+import ssl
 
 class URL: 
     def __init__(self, url):
         self.scheme, url = url.split("://", 1)
-        assert self.scheme == "http"
+        assert self.scheme in [ "http", "https" ]
+
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
 
         if '/' not in url:
             self += "/"
         self.host, url = url.split("/", 1)
         self.path = "/" + url
+
+        # Custom port support
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
+        
     # Method won't work if you're offline / behing a proxy / cmoplex environment
     def request(self):
         # Socket for communicating with server
+        ctx = ssl.create_default_context()
         s = socket.socket(
             family=socket.AF_INET,
             type=socket.SOCK_STREAM,
             proto=socket.IPPROTO_TCP,
         )
-        s.connect((self.host, 80))
+        s.connect((self.host, self.port))
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
 
         request = "GET {} HTTP/1.0\r\n".format(self.path)
         request += "Host: {}\r\n".format(self.host)
